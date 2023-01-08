3e669e20de09a59fc6e743a9ab8467d71bec75c7
yurii_yanchuk <yurii_yanchuk@epam.com>
2023-01-08 21:42:52 +0200

    Add realisation via Field Injection with @Value from properties

--- src/test/java/ua/yanchuk/spring/springselenium/SpringSeleniumApplicationTests.java
+++ src/test/java/ua/yanchuk/spring/springselenium/SpringSeleniumApplicationTests.java
@@ -2,7 +2,10 @@ package ua.yanchuk.spring.springselenium;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringSeleniumApplicationTests {
@@ -10,10 +13,27 @@ class SpringSeleniumApplicationTests {
     @Autowired
     private User user;
 
+//    @Value("${PATH}")
+//    private String path;
+
+    @Value("${fruits}")
+    private List<String> fruits;   // Spring automatically converts variable type from property
+
+    @Value("${timeout}")
+    private int timeout;
+
+    @Value("${myusername}")
+    private String username;
+
     @Test
     void contextLoads() {
 
-        user.printDetails();
+        System.out.println(this.fruits);
+        System.out.println(this.fruits.size());
+        System.out.println(this.timeout);
+        System.out.println(this.username);
+
+//        user.printDetails();
     }
 
 	/*
