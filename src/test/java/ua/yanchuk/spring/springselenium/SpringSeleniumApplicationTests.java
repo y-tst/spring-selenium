eec9a37d7bcd44c3beed2e4d4e083b329efeda1b
yurii_yanchuk <yurii_yanchuk@epam.com>
2023-01-08 16:02:33 +0200

    Initial commit

---
+++ src/test/java/ua/yanchuk/spring/springselenium/SpringSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.yanchuk.spring.springselenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
