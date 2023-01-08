5b09ea7f5399e9167ac44daf83b9fa5fcf31c488
yurii_yanchuk <yurii_yanchuk@epam.com>
2023-01-08 19:51:42 +0200

    Add realisation via Setter Methods

--- src/test/java/ua/yanchuk/spring/springselenium/User.java
+++ src/test/java/ua/yanchuk/spring/springselenium/User.java
@@ -1,5 +1,6 @@
 package ua.yanchuk.spring.springselenium;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,8 +9,20 @@ public class User {
     private Address address;
     private Salary salary;
 
+    /*
     public User(Address address, Salary salary) {
         this.address = address;
+        this.salary = salary;
+    }
+     */
+
+    @Autowired
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Autowired
+    public void setSalary(Salary salary) {
         this.salary = salary;
     }
 
