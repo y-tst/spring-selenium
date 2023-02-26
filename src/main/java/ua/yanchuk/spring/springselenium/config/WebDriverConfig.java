1a1b3b3e4d33a83dd90190532f7584edf7ebe893
yurii_yanchuk <yurii_yanchuk@epam.com>
2023-02-26 23:53:48 +0200

    Add @ConditionalOnProperty

--- src/main/java/ua/yanchuk/spring/springselenium/config/WebDriverConfig.java
+++ src/main/java/ua/yanchuk/spring/springselenium/config/WebDriverConfig.java
@@ -3,10 +3,13 @@ package ua.yanchuk.spring.springselenium.config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.time.Duration;
 
@@ -17,9 +20,17 @@ public class WebDriverConfig {
     private int timeout;
 
     @Bean
+    @ConditionalOnProperty(name = "browser", havingValue = "chrome")
     public WebDriver chromeDriver() {
         WebDriverManager.chromedriver().driverVersion("108.0.5359.125").setup();
         return new ChromeDriver();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "browser", havingValue = "firefox")
+    public WebDriver firefoxDriver() {
+        WebDriverManager.firefoxdriver().driverVersion("108.0.2 (64-bit)").setup();
+        return new FirefoxDriver();
     }
 
     @Bean
